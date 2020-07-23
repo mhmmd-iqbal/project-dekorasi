c3f7f73d9ff2644f3bf054ca6d74368fed904c9f
mhmmd-iqbal <mhmmd.iqbal.june@gmail.com>
2020-07-23 21:36:14 +0700

    second commit

---
+++ app/Views/login.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Halaman Login User</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="<?= base_url() ?>/assets/template/main/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/template/main/login.css">
+</head>
+
+
+<body>
+
+    <div class="login-block">
+        <h1>
+            <a href="/"> Informasi Jasa Dekorasi Aceh</a>
+        </h1>
+        <form action="/Login/SignIn" method="POST">
+            <?= csrf_field() ?>
+            <input type="text" value="" placeholder="Username" id="username" />
+            <input type="password" value="" placeholder="Password" id="password" />
+            <button type="submit"><i class="fa fa-sign-in"></i> Masuk</button>
+        </form>
+    </div>
+</body>
+
+</html>
